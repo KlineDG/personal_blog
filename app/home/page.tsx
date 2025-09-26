a484463751ebbef14a64bb606198a4e9b9dd799d
Dylan <Klinedg9@icloud.com>
2025-09-26 13:50:28 -0600

    first commit

---
+++ app/home/page.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return <div className="">Home Page</div>;
+}
+
+// colors bg-main #121212 text-main #eaeaea accent #d4afe3 logo-font Libre Barcode 39 Text
+
+export default Home;
